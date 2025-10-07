45835def0c0c33a4b4198a21887bf45843d4ad0a
김성현 <oilater@naver.com>
2025-10-08 02:11:37 +0900

    feat: Middleware 구현

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,19 +1,34 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getAllCats(): string {
+    throw new HttpException('api is broken', HttpStatus.FORBIDDEN);
     return 'all cats';
   }
 
   // cats/:id
   @Get(':id')
-  getOneCat(): string {
-    return 'one cat';
+  getOneCat(@Param('id', ParseIntPipe) param: number): string {
+    console.log(typeof param);
+    return `one cat: ${param}`;
   }
 
   @Post()
--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,8 +1,17 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  private logger = new Logger('HTTP');
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      this.logger.log(`
+        RESPONSE STATUS: ${res.statusCode}
+        IP: ${req.ip} METHOD: ${req.method}
+        URL: ${req.originalUrl}
+      `);
+    });
     next();
   }
 }
